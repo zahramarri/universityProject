0742c32b5eed410125bf0a0950ebc1b3941dac7d
zahramarri <zahramarri.dev@gmail.com>
2022-06-30 20:13:39 +0430

    add some property to object University

--- src/main/kotlin/University.kt
+++ src/main/kotlin/University.kt
@@ -1,2 +1,5 @@
-class University {
+object University {
+    val listOfProfessors: MutableList<Professor> = mutableListOf()
+    val listOfStudents: MutableList<Student> = mutableListOf()
+    val listOfMages: MutableList<Mager> = mutableListOf()
 }
